01c98d481ae66cc848ad8373632f9e62e343dd58
zhuwentao1 <zhuwentao1@corp.netease.com>
2022-04-29 19:07:45 +0800

    feat:initial

---
+++ src/main/java/Jenkins/Jenkins.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)Jenkins, 2022/4/29.
+ * <p>
+ * <p>
+ * Copyright 2022 Netease, Inc. All rights reserved.
+ * NETEASE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package Jenkins;
+
+/**
+ * @author zhuwentao1@corp.netease.com
+ */
+public class Jenkins {
+
+    static {
+        System.out.println("Jenkins!!");
+    }
+}
